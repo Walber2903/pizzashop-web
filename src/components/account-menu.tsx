b08dec8effcfa00dd676789d1bce13d749938677
Walber <walber.araujo@getcandidly.com>
2024-01-22 19:46:49 -0300

    added header with menu, theme toggle and dropdown to logout

---
+++ src/components/account-menu.tsx
@@ -0,0 +1,44 @@
+import { DropdownMenuTrigger } from '@radix-ui/react-dropdown-menu'
+import { Building, ChevronDown, LogOut } from 'lucide-react'
+
+import { Button } from './ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+} from './ui/dropdown-menu'
+
+export function AccountMenu() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="outline"
+          className="flex select-none items-center gap-2"
+        >
+          Pizza Shop
+          <ChevronDown className="h-4 w-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="w-56">
+        <DropdownMenuLabel className="flex flex-col">
+          <span>Walber Araujo</span>
+          <span className="text-xs font-normal text-muted-foreground">
+            walber.menezes@hotmail.com
+          </span>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <Building className="mr-2 h-4 w-4" />
+          <span>Perfil da Loja</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem className="text-rose-600 dark:text-rose-400">
+          <LogOut className="mr-2 h-4 w-4" />
+          <span>Sair</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
